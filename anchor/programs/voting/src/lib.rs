fb4ac55cded73cb5380da2523f68aaf53262a256
Andriy Zhdanov <azhdanov@gmail.com>
2025-04-07 10:24:49 +0200

    Initialize Candidate

--- anchor/programs/voting/src/lib.rs
+++ anchor/programs/voting/src/lib.rs
@@ -18,6 +18,16 @@ pub mod votingdapp {
         msg!("Poll initialized with ID: {}", poll_id);
         Ok(())
     }
+
+    pub fn initialize_candidate(ctx: Context<InitializeCandidate>,
+        candidate_name: String, poll_id: u64) -> Result<()> {
+        let candidate = &mut ctx.accounts.candidate;
+        candidate.poll_id = poll_id;
+        candidate.candidate_name = candidate_name.clone();
+        candidate.candidate_votes = 0;
+        msg!("Candidate initialized with name: {}", candidate_name);
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
@@ -45,4 +55,34 @@ pub struct Poll {
     pub poll_start: u64,
     pub poll_end: u64,
     pub candidate_amount: u64,
+}
+
+#[derive(Accounts)]
+#[instruction(candidate_name: String, poll_id: u64)]
+pub struct InitializeCandidate<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(
+        seeds = [ poll_id.to_le_bytes().as_ref() ],
+        bump,
+    )]
+    pub poll: Account<'info, Poll>,
+    #[account(
+        init,
+        payer = signer,
+        space = 8 * Candidate::INIT_SPACE,
+        seeds = [ poll_id.to_le_bytes().as_ref(), candidate_name.as_ref() ],
+        bump,
+    )]
+    pub candidate: Account<'info, Candidate>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+# [derive(InitSpace)]
+pub struct Candidate {
+    #[max_len(280)]
+    pub candidate_name: String,
+    pub candidate_votes: u64,
+    pub poll_id: u64,
 }
--- anchor/tests/votingdapp.spec.ts
+++ anchor/tests/votingdapp.spec.ts
@@ -10,6 +10,10 @@ describe('votingdapp', () => {
   anchor.setProvider(provider)
 
   const program = anchor.workspace.Votingdapp as Program<Votingdapp>
+  const voitingAdress = program.programId
+
+  beforeAll(async () => {
+  })
 
   it('Initialize Votingdapp', async () => {
     console.log("Calling initialize");
@@ -19,5 +23,31 @@ describe('votingdapp', () => {
 				signer: provider.wallet.publicKey,
 			})
       .rpc()
+    const [pollAddress] = await PublicKey.findProgramAddressSync(
+      [new anchor.BN(1).toArrayLike(Buffer, 'le', 8)],
+      voitingAdress
+    )
+    const poll = await program.account.poll.fetch(pollAddress)
+    expect(poll.pollId.toString()).toEqual(new anchor.BN(1).toString())
+    expect(poll.description).toEqual("Test Poll")
+    expect(poll.pollStart.toNumber()).toBeLessThan(poll.pollEnd.toNumber())
+    console.log("Poll: ", poll)
+  })
+
+  it('Initialize Candidate', async () => {
+    console.log("Calling initialize candidate");
+    await program.methods
+      .initializeCandidate("Test Candidate", new anchor.BN(1))
+      .rpc()
+    const [pollAddress] = await PublicKey.findProgramAddressSync(
+      [new anchor.BN(1).toArrayLike(Buffer, 'le', 8), Buffer.from("Test Candidate")],
+      voitingAdress
+    )
+    const candidate = await program.account.candidate.fetch(pollAddress)
+    expect(candidate.candidateName).toEqual("Test Candidate")
+    console.log("Candidate: ", candidate)
+  })
+
+  it('Vote', async () => {
   })
 })
